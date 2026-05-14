cb8ac52f2c053bcc076d84f5e33a85243a1d9b2a
SuspiciousApplePie <andreiusita6@gmail.com>
2026-05-14 23:40:50 +0800

    Add link list factory

--- js/hashmap.js
+++ js/hashmap.js
@@ -40,3 +40,49 @@ export function Node(value = null, key = null, nextNode = null) {
     nextNode,
   };
 }
+
+export function LinkedList() {
+  let head = null;
+  let tail = null;
+  return {
+    append: (node) => {
+      if (!head) {
+        head = node;
+        tail = node;
+      } else {
+        tail.nextNode = node;
+        tail = node;
+      }
+    },
+
+    findValue: (key) => {
+      let cur = head;
+      let value = null;
+      while (cur) {
+        if (cur.key === key) {
+          value = cur.value;
+          break;
+        } else {
+          cur = cur.nextNode;
+        }
+      }
+
+      return value;
+    },
+
+    update(key, value) {
+      let cur = head;
+      let isDuplicate = false;
+      while (cur) {
+        if (key === cur.key) {
+          cur.value = value;
+          isDuplicate = true;
+          break;
+        } else {
+          cur = cur.nextNode;
+        }
+      }
+      return isDuplicate;
+    },
+  };
+}
