83158a975c6104b39fca70cee7da0e14ccada8ad
SuspiciousApplePie <andreiusita6@gmail.com>
2026-05-16 20:43:23 +0800

    Add main js file

---
+++ js/main.js
@@ -0,0 +1,38 @@
+import { HashMap } from "./hashmap.js";
+
+const test = HashMap();
+
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+
+console.log(test.entries());
+
+test.set("lion", "bronze");
+test.set("hat", "blue");
+test.set("apple", "cider");
+test.set("grape", "fruit");
+
+console.log(test.entries());
+
+test.set("moon", "silver");
+
+console.log(test.length());
+
+test.set("lion", "king");
+test.set("hat", "amethyst");
+test.set("apple", "pie");
+test.set("grape", "veggie");
+
+console.log(test.entries());
+console.log(test.get("apple"));
+console.log(test.has("carrot"));
